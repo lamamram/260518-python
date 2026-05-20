ffef0f28c46c0eef03aff42ac05c3902d545f621
mlamamra <mlamamra@dawan.fr>
2026-05-20 08:44:35 +0200

    j2 : corrections

--- cours_02_sequences.py
+++ cours_02_sequences.py
@@ -9,6 +9,9 @@ nom = "doe"
 # surcharge de l'opérateur + : concaténation de str
 # pas de conversion implicite en python
 
+print(prenom + " " + nom) # surcharge de l'opérateur + : concaténation de str
+print(prenom + nom + str(2)) # pas de conversion implicite en python
+
 # %% ---------- fonctions internes d'une variable selon son type : ici str ----
 
 ######### <variable>.<fonction>()
@@ -17,18 +20,20 @@ prenom = "john"
 nom = "doe"
 
 # afficher le nom complet : John DOE
-
+print(prenom.capitalize() + " " + nom.upper()) # capitalize : 1ère lettre en majuscule, upper : tout en majuscules
 # %% ------------ indéxer une séquence ------------------------------------
 nom_complet = "Joe DOE"
 
 # prendre la longueur d'une str
+print(len(nom_complet))
 
 ######## <variable>[n]: caractère de la variable à la position n 
 
 # afficher le 1er caractère de la str, le 1er car. du 2ème mot, le dernier car.
-
+print(nom_complet[0], nom_complet[4], nom_complet[len(nom_complet) -1], nom_complet[-1])
 
 # %% ------------- SLICING d'une séquence ----------------------------------- 
+
 
 civilite_nom_complet = "Mr. Joe DOE"
 
@@ -38,10 +43,11 @@ civilite_nom_complet = "Mr. Joe DOE"
 # afficher la civilité, le prénom, le nom à partir de civilite_nom_complet
 
 # slicing: indice de début compris: indice de fin non compris, début == 0 ou rien, fin => rien si dernier
-
+print(civilite_nom_complet[:3], civilite_nom_complet[4:7], civilite_nom_complet[8:])
 # afficher une chaine contenant chaque 1er car. de chaque mot //
-
+print(civilite_nom_complet[::4])
 # retourner civilite_nom_complet
+print(civilite_nom_complet[::-1])
 
 
 
@@ -77,11 +83,49 @@ mots = ["appeler", "un", "chat"]
 
 # afficher chat à partir de mots
 
+mots[2]
 
 # modifier mots en concaténant ["il", "faut"] , mots et ["un", "chat"]
 
+mots = ["il", "faut"] + mots + ["un", "chat"]
 
 # remettre mots dans sa valeur initiale à partir de sa valeur courante
+
+mots = mots[2:5]
+print(mots)
+
+# %% ---------------- fonctions internes exclusives aux listes -----------------
+
+mots = ["appeler", "un"]
+
+# ajouter "chat" à droite avec [] ou autrement
+# mots[2] = "chat" # [] : IndexError
+
+mots.append("chat") # append : ajoute un élément à la fin de la liste
+
+# ajouter ["chat", "un", "chat", "gris"] à droite
+mots.extend(["chat", "un", "chat", "gris"]) # extend : ajoute les éléments d'une liste à la fin de la liste courante
+
+# ajouter "il" à gauche
+mots.insert(0, "il") # insert : ajoute un élément à une position donnée dans la liste, 0 : début de la liste
+
+# ajouter "faut" à gauche d'"appeler"
+
+mots.insert(1, "faut")
+
+# supprimer le dernier élement de mots et retourne sa valeur
+
+mots.pop() # pop : supprime et retourne le dernier élément de la liste
+
+# supprimer le premier élément //
+
+mots.pop(0)
+
+# supprimer la 1ère occurence de chat 
+
+mots.remove("chat")
+
+print(mots)
 
 
 # %% -------------- transformation de liste <=> chaîne de caractères ----------
@@ -90,9 +134,12 @@ civilite_nom_complet = "Mr. Joe DOE"
 
 # créer la liste mots, des mots de civilite_nom_complet => split
 
+mots = civilite_nom_complet.split(" ")
 
 # recréer civilite_nom_complet à partir de mots
 # en soudant les élements de mots avec un " " => join
+
+civilite_nom_complet = " ".join(mots)
 
 
 # %% ---------------- fonctions internes exclusives aux listes -----------------
@@ -149,4 +196,6 @@ print("chien" not in phrase)
 # idem en transformant phrase en mots
 mots = phrase.split()
 print("chat" in mots, mots.count("chat"))
+
 # %%
+
--- exo_01_calcul_moyenne.py
+++ exo_01_calcul_moyenne.py
@@ -10,3 +10,39 @@ que ces valeurs sont des entiers relatifs (1er cas entier naturel ensuite 2ème 
 """
 
 # %%
+# donées d'entrées
+valeurs = input("saisir des entiers relatifs séparés par ,:")
+# écrire une variable en dernière ligne d'une cellule et au niveau d'indentation 0 la rend affichable dans la console jupyter 
+valeurs
+
+# %%
+candidates = valeurs.split(",")
+candidates
+
+# %%
+# initialiser les listes pour traiter les saisies
+integers, errors = [], []
+for candidat in candidates:
+  # nettoyer les espaces à gauche et à droite
+  candidat = candidat.strip()
+  # la chaine de caractère représente des chiffres OU (une chaine qui commence avec "-" ET le reste est fait de chiffres)
+  # ==> représente un entier relatif DONC convertible
+  if candidat.isnumeric() or (candidat.startswith("-") and candidat[1:].isnumeric()):
+    integers.append(int(candidat))
+  else:
+    if candidat:
+      errors.append(candidat)
+  
+integers
+
+# %%
+# s'il n'y a pas d'erreurs ==> si errors est la liste vide []
+if not errors:
+  # s'il ya des integers ==> si intergers contient des nombres ==> n'est PAS []
+  if integers:
+    avg = round(sum(integers) / len(integers), 2)
+    print(f"moyenne à 2 décimales sign. de { integers }: {avg}")
+  else:
+    print("liste vide !!!!")
+else:
+  print(f"valeurs défectueuses: {errors}")

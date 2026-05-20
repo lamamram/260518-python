ca9e9cb67449b6031f31588d5cd0645411a1cbd8
mlamamra <mlamamra@dawan.fr>
2026-05-20 16:35:04 +0200

    modules & librairies standard

--- cours_06_modules_packages.py
+++ cours_06_modules_packages.py
@@ -7,20 +7,28 @@
 ##    est appelé module principal
 
 # 1. créer un module tools qui contient la fonction de l'exercice template
+import tools
 
 
 # 2. importer le module est exécuter la fonction dans le module principal
 # tools est une variable de type module qu'on appelle espace de nom du module
 
+tpl = "machin: {{clef}}"
+dico = {"clef": 33}
+print(tools.parse_template(tpl, dico))
+
 # %% -- à partir d'un module, importer une fonction => sans espace de nom ----
 # idem sans espace de nom
+from tools import parse_template as parse_tpl
 
-
-# %% -- gérer les conflits de noms => alias
+# gérer les conflits de noms => alias
 # idem en changeant le nom de la fonction à l'import
 
 parse_template = "qqch d'autre"
 
+tpl = "machin: {{clef}}"
+dico = {"clef": 33}
+print(parse_tpl(tpl, dico))
 
 # %% --- exemples d'utilisation de modules de la bibliothèque standard ------
 # exemple de datetime
@@ -30,19 +38,27 @@ from datetime import datetime, timedelta
 
 # créer une variable dt représente la date d'aujourd'hui avec la signature de base 
 
-dt = datetime(2026, 4, 1, 15, 30)
+dt = datetime(2026, 5, 20, 16, 18)
+dt
 # idem avec une fonction interne
 
+dt = datetime.now()
+dt
 
-# idem à partir de la chaine de caractère "2026-04-01 15:30" et le format "%Y-%m-%d %H:%M" 
+# idem à partir de la chaine de caractère "2026-05-20 16:21" et le format "%Y-%m-%d %H:%M" 
+dt = datetime.strptime("2026-05-20 16:21", "%Y-%m-%d %H:%M")
+dt
 
 # à partir de la variable, afficher l'année, la date au format "%d/%m/%Y", le nb de secondes à partir le 1er janvier 1970
-
+dt.year, dt.strftime("%d/%m/%Y"), dt.timestamp()
 
 # afficher la durée entre la fin de la journée et maintenant, en heures, min, s
+delta = datetime(2026, 5, 20, 17) - datetime.now()
 
+delta.seconds // 3600, delta.seconds % 3600 // 60, delta.seconds % 60  
 # date + duree (timedelta) = date
 
+datetime.now() + timedelta(minutes=3)
 # %% ------------- import d'un module d'un package ------------------------
 
 # ici le "." on appelle çà le "chemin python": Python Path (comme "/" ou "\")

4a86f00028829094380377e3e6f41073f1acd56b
mlamamra <mlamamra@dawan.fr>
2026-05-20 14:52:31 +0200

    portage d'un bout de code en fonction

--- exo_02_templating.py
+++ exo_02_templating.py
@@ -78,17 +78,20 @@ def parse_template(
     values: dict, 
     sep_in: str="{{", 
     sep_out: str="}}", 
-    default: str="N/A"
+    default: str="N/A",
+    **opts
   ):
   for _ in range(tpl.count(sep_in)):
     index_start = tpl.index(sep_in) + len(sep_in)
     index_end = tpl.index(sep_out)
     key = tpl[index_start:index_end]
+    if "debug" in opts and opts["debug"]:
+      print(f"debug: key: {key}")
 
     tpl = tpl.replace(sep_in + key + sep_out, str(values.get(key, default)))
   return tpl
 
 print(parse_template(_template, injections, sep_out="))", sep_in="(("))
 
-print(parse_template("machin: {{key}}", {"key": 33}))
+print(parse_template("machin: {{clef}}", {"clef": 33}))
 # %%

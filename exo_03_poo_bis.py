925d07ec32b57fb19612824acb5c4844b2d69b17
mlamamra <mlamamra@dawan.fr>
2026-05-21 10:50:24 +0200

    j4: exo poo

--- exo_03_poo_bis.py
+++ exo_03_poo_bis.py
@@ -3,9 +3,9 @@
 2/ créer un module "misc.py" dans le package
 3/ dans misc.py, copier la classe Account
 4/ créer une classe Client qui contient
-   - un prénom, un nom, et une date en str et un format de date
-   - l'initialisation des objets utilise 3 attributs
-     + prénom, nom, un objet date qui représente la date d'inscription
+   - 3 attributs: un prénom, un nom, un objet date qui représente la date d'inscription
+   - l'initialisation des objets utilise 4 attributs
+     + prénom, nom, une date en str et un format de date
    - une méthode qui retourne le nom complet du client avec des majuscules
 5/ ???
 6/ tester le programme principal dans ce module courant

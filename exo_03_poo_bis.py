4aa7af74253909372bb1cca0df7531b374a1a62d
mlamamra <mlamamra@dawan.fr>
2026-05-21 10:47:48 +0200

    j4: exo poo

---
+++ exo_03_poo_bis.py
@@ -0,0 +1,14 @@
+"""
+1/ créer un package "bank" dans le sous dossier workdir
+2/ créer un module "misc.py" dans le package
+3/ dans misc.py, copier la classe Account
+4/ créer une classe Client qui contient
+   - un prénom, un nom, et une date en str et un format de date
+   - l'initialisation des objets utilise 3 attributs
+     + prénom, nom, un objet date qui représente la date d'inscription
+   - une méthode qui retourne le nom complet du client avec des majuscules
+5/ ???
+6/ tester le programme principal dans ce module courant
+"""
+
+# %%
